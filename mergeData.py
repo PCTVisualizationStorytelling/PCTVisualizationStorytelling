25a3d73b66d755fc5e5a12b581b8574b07b6e319
Kusnick <kusnick@imada.sdu.dk>
2025-04-25 00:29:55 +0200

    New files

--- mergeData.py
+++ mergeData.py
@@ -1,84 +1,70 @@
 import pandas as pd
 
-filename = "Segmented_PCT_45_Segments.csv"
+file_numbers = [20, 25, 30, 35, 40, 45]
 
-# Load the files
-pct_30 = pd.read_csv(filename)
-pct_50 = pd.read_csv("Segmented_PCT_50_Segments.csv")
+filename_default = "Segmented_PCT_NUMBER_Segments.csv"
 
-# Clean up column names (remove leading/trailing whitespace)
-pct_30.columns = pct_30.columns.str.strip()
-pct_50.columns = pct_50.columns.str.strip()
+def merge_file(filename):
 
-print("Columns")
-print(pct_30.columns.difference(pct_50.columns))
-print(pct_50.columns.difference(pct_30.columns))
-print()
+    # Load the files
+    pct_30 = pd.read_csv(filename)
+    pct_50 = pd.read_csv("Segmented_PCT_50_Segments.csv")
 
-try:
-    pct_30 = pct_30.reset_index()  # make sure indexes pair with number of rows
-except:
-    pass
+    # Clean up column names (remove leading/trailing whitespace)
+    pct_30.columns = pct_30.columns.str.strip()
+    pct_50.columns = pct_50.columns.str.strip()
 
-try:
-    pct_50 = pct_50.reset_index()  # make sure indexes pair with number of rows
-except:
-    pass
+    print("Columns")
+    print(pct_30.columns.difference(pct_50.columns))
+    print(pct_50.columns.difference(pct_30.columns))
+    print()
 
-merged = pd.merge(
-    pct_30,
-    pct_50,
-    on=["Start Longitude", "Start Latitude"]
-)
+    try:
+        pct_30 = pct_30.reset_index()  # make sure indexes pair with number of rows
+    except:
+        pass
 
-print(merged)
+    try:
+        pct_50 = pct_50.reset_index()  # make sure indexes pair with number of rows
+    except:
+        pass
 
-last_index = 0
-merged_rows = []
-for index, row in merged.iterrows():
-    index_y = row["index_y"]
-    collection = []
-    photo = None
+    try:
+        pct_30.insert(13, "Description_new", "")
+    except:
+        pass
 
-    df1 = pct_50.iloc[last_index:index_y]
-    for mergingindex, mergingrow in df1.iterrows():
-        collection.append(mergingrow["Description"].strip())
-        if photo is None:
-            photo = mergingrow["imagesPath"]
+    try:
+        pct_30.insert(14, "imagesPath", "")
+    except:
+        pass
 
-    print(df1)
-    print(collection)
-    if len(collection) > 0:
-        merged_rows.append({"rows": collection, "photo": photo})
+    print(pct_30.columns)
 
-    last_index = index_y
+    for mergingindex, mergingrow in pct_30.iterrows():
+        description = mergingrow["Description"]
+        pct_30.at[mergingindex, 'Description_new'] = mergingrow["Description"]
 
-collection = []
-photo = None
-df1 = pct_50.iloc[last_index:]
-for mergingindex, mergingrow in df1.iterrows():
-    collection.append(mergingrow["Description"].strip())
-    if photo is None:
-        photo = mergingrow["imagesPath"]
+        photo = None
+        for biggerindex, biggerrow in pct_50.iterrows():
+            if biggerrow["Description"] in description:
+                pct_30.at[mergingindex, 'Description_new'] = pct_30.at[mergingindex, 'Description_new'].replace(biggerrow["Description"], biggerrow["Description_new"]).replace(",", ";")
+                pct_30.at[mergingindex, 'Description'] = pct_30.at[mergingindex, 'Description'].replace(",", ";")
+                if photo is None:
+                    photo = biggerrow["imagesPath"]
+                pct_30.at[mergingindex, 'imagesPath'] = photo
 
-merged_rows.append({"rows": collection, "photo": photo})
+    try:
+        pct_30 = pct_30.drop('index', axis=1)
+    except:
+        pass
 
-print()
-print(len(merged_rows))
-print(merged_rows)
+    try:
+        pct_30 = pct_30.drop('level_0', axis=1)
+    except:
+        pass
 
-for index, row in pct_30.iterrows():
-    pct_30.at[index, 'Description'] = " ".join(merged_rows[index]["rows"])
-    pct_30.at[index, 'imagesPath'] = merged_rows[index]["photo"]
+    pct_30.to_csv("merged/"+filename, index=False)
 
-try:
-    pct_30 = pct_30.drop('index', axis=1)
-except:
-    pass
-
-try:
-    pct_30 = pct_30.drop('level_0', axis=1)
-except:
-    pass
-
-pct_30.to_csv("merged/"+filename, index=False)
+for nm in file_numbers:
+    merge_file(filename_default.replace("NUMBER", str(nm)))
